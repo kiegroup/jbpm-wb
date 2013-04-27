67a8b740159961b224f78603af4762fd6815d8ce
Petr Siroky <psiroky@redhat.com>
2013-04-27 23:58:46 +0200

    JBPM-3988: add tests for 'byDays' task queries

---
+++ jbpm-console-ng-human-tasks/jbpm-console-ng-human-tasks-backend/src/test/java/org/jbpm/console/ng/ht/backend/server/TaskServiceEntryPointLocalTest.java
@@ -0,0 +1,54 @@
+package org.jbpm.console.ng.ht.backend.server;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ArchivePaths;
+import org.jboss.shrinkwrap.api.Filters;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.runner.RunWith;
+
+@RunWith(Arquillian.class)
+public class TaskServiceEntryPointLocalTest extends TaskServiceEntryPointBaseTest {
+
+    @Deployment()
+    public static Archive<?> createDeployment() {
+        return ShrinkWrap
+                .create(JavaArchive.class, "jbpm-console-ng-human-task-cdi.jar")
+                .addPackage("org.jboss.seam.persistence")
+                // seam-persistence
+                .addPackage("org.jboss.seam.transaction")
+                // core jbpm
+                .addPackage("org.jbpm.shared.services.api")
+                .addPackage("org.jbpm.shared.services.impl")
+                .addPackage("org.jbpm.services.task")
+                .addPackage("org.jbpm.services.task.annotations")
+                .addPackage("org.jbpm.services.task.api")
+                .addPackage("org.jbpm.services.task.impl")
+                .addPackage("org.jbpm.services.task.impl.model")
+                .addPackage("org.jbpm.services.task.events")
+                .addPackage("org.jbpm.services.task.exception")
+                .addPackage("org.jbpm.services.task.identity")
+                .addPackage("org.jbpm.services.task.factories")
+                .addPackage("org.jbpm.services.task.internals")
+                .addPackage("org.jbpm.services.task.internals.lifecycle")
+                .addPackage("org.jbpm.services.task.lifecycle.listeners")
+                .addPackage("org.jbpm.services.task.query")
+                .addPackage("org.jbpm.services.task.util")
+                .addPackage("org.jbpm.services.task.deadlines")
+                // deadlines
+                .addPackage("org.jbpm.services.task.deadlines.notifications.impl")
+                .addPackage("org.jbpm.services.task.subtask")
+                // console-ng task service
+                .addPackage("org.jbpm.console.ng.ht.service")
+                .addPackages(true, Filters.exclude(FormServiceEntryPointImpl.class), "org.jbpm.console.ng.ht.backend.server")
+                // .addPackage("org.jbpm.services.task.commands") // This should not be
+                // required here
+                .addAsManifestResource("META-INF/persistence.xml",
+                        ArchivePaths.create("persistence.xml"))
+                .addAsManifestResource("META-INF/Taskorm.xml",
+                        ArchivePaths.create("Taskorm.xml"))
+                .addAsManifestResource("META-INF/beans.xml", ArchivePaths.create("beans.xml"));
+    }
+}
