f850d9e2b5ec723239fc5a8c30bf4da1004f92c3
Horacio Antar <horacioantar@gmail.com>
2013-06-17 21:19:41 -0300

    Add clases base del modulo History

---
+++ jbpm-console-ng-human-tasks/jbpm-console-ng-human-tasks-client/src/main/java/org/jbpm/console/ng/ht/client/history/HistoryPresenter.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2013 JBoss Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jbpm.console.ng.ht.client.history;
+
+import javax.enterprise.context.Dependent;
+import javax.enterprise.event.Observes;
+
+@Dependent
+/* @WorkbenchScreen(identifier = "Histories List") */
+public class HistoryPresenter {
+    
+
+    public void saveHistory(@Observes @History PointHistory pointHistory) {
+        System.out.println("*********@Observes @History***************");
+    }
+
+    // public interface HistoryListView extends UberView<HistoryPresenter> {
+    //
+    // void displayNotification( String text );
+    //
+    // TaskListMultiDayBox getTaskListMultiDayBox();
+    //
+    // MultiSelectionModel<TaskSummary> getSelectionModel();
+    //
+    // TextBox getSearchBox();
+    //
+    // void refreshTasks();
+    // }
+
+}
