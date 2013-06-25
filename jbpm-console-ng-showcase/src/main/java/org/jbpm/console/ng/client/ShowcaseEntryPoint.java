241e575b9b8617ff2051ee76008080d40e028163
romartin <romartin@redhat.com>
2013-06-25 12:15:32 +0200

    Fixed dashbuilder link url

--- jbpm-console-ng-showcase/src/main/java/org/jbpm/console/ng/client/ShowcaseEntryPoint.java
+++ jbpm-console-ng-showcase/src/main/java/org/jbpm/console/ng/client/ShowcaseEntryPoint.java
@@ -184,7 +184,7 @@ public class ShowcaseEntryPoint {
         result.add( MenuFactory.newSimpleItem( constants.Process_Dashboard() ).respondsWith( new Command() {
             @Override
             public void execute() {
-                Window.open( "http://localhost:8080/dashbuilder/workspace/jbpm-dashboard", "_blank", "" );
+                Window.open( "/dashbuilder/workspace/jbpm-dashboard", "_blank", "" );
             }
         } ).endMenu().build().getItems().get( 0 ) );
 
