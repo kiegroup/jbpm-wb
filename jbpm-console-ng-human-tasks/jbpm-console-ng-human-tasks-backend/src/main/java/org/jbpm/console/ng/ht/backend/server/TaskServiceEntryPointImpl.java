d9c241ca4baba574f3d2f7b3b941e8bd9e2ab1e0
Horacio Antar <horacioantar@gmail.com>
2013-07-03 17:21:34 -0300

    change in javadoc

--- jbpm-console-ng-human-tasks/jbpm-console-ng-human-tasks-backend/src/main/java/org/jbpm/console/ng/ht/backend/server/TaskServiceEntryPointImpl.java
+++ jbpm-console-ng-human-tasks/jbpm-console-ng-human-tasks-backend/src/main/java/org/jbpm/console/ng/ht/backend/server/TaskServiceEntryPointImpl.java
@@ -5,14 +5,14 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *       http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
-*/
+ */
 package org.jbpm.console.ng.ht.backend.server;
 
 import java.text.SimpleDateFormat;
@@ -245,7 +245,7 @@ public class TaskServiceEntryPointImpl implements TaskServiceEntryPoint {
 
     /**
      * Group Operations
-    */
+     */
     public List<TaskSummary> getTasksAssignedByGroup(String userId, String groupId, String language) {
         List<org.kie.api.task.model.TaskSummary> tasksAssignedAsPotentialOwner = taskService.getTasksAssignedAsPotentialOwner(
                 userId, language);
