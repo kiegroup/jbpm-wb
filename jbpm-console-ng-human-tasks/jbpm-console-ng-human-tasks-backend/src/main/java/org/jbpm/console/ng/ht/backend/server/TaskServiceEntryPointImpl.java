fdf1a6937d2d232e2a4c3a36d99002fefda77724
Horacio Antar <horacioantar@gmail.com>
2013-07-03 17:03:23 -0300

    change in javadoc

--- jbpm-console-ng-human-tasks/jbpm-console-ng-human-tasks-backend/src/main/java/org/jbpm/console/ng/ht/backend/server/TaskServiceEntryPointImpl.java
+++ jbpm-console-ng-human-tasks/jbpm-console-ng-human-tasks-backend/src/main/java/org/jbpm/console/ng/ht/backend/server/TaskServiceEntryPointImpl.java
@@ -1,17 +1,17 @@
 /*
-* Copyright 2012 JBoss by Red Hat.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
+ * Copyright 2012 JBoss by Red Hat.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
 */
 package org.jbpm.console.ng.ht.backend.server;
 
@@ -129,8 +129,8 @@ public class TaskServiceEntryPointImpl implements TaskServiceEntryPoint {
     }
 
     /**
-* Day adaptors
-*/
+     * Day adaptors
+     */
     public Map<Day, List<TaskSummary>> getTasksOwnedFromDateToDateByDays(String userId, List<String> strStatuses,
             Date dateFrom, Date dateTo, String language) {
         LocalDate dayFrom = new LocalDate(dateFrom);
@@ -244,8 +244,8 @@ public class TaskServiceEntryPointImpl implements TaskServiceEntryPoint {
 
 
     /**
-* Group Operations
-*/
+     * Group Operations
+    */
     public List<TaskSummary> getTasksAssignedByGroup(String userId, String groupId, String language) {
         List<org.kie.api.task.model.TaskSummary> tasksAssignedAsPotentialOwner = taskService.getTasksAssignedAsPotentialOwner(
                 userId, language);
