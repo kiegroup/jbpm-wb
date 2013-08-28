17606d15fa3841977113453ea78ed272e73a35e0
Maciej Swiderski <mswiders@redhat.com>
2013-08-28 12:30:27 +0200

    BZ-1000592 - Provide the user with an information message why 'build&deploy' failed

--- jbpm-console-ng-business-domain/jbpm-console-ng-business-domain-client/src/main/java/org/jbpm/console/ng/bd/client/perspectives/DeploymentListPerspective.java
+++ jbpm-console-ng-business-domain/jbpm-console-ng-business-domain-client/src/main/java/org/jbpm/console/ng/bd/client/perspectives/DeploymentListPerspective.java
@@ -26,8 +26,11 @@ import org.uberfire.lifecycle.OnStartup;
 import org.uberfire.client.annotations.Perspective;
 import org.uberfire.client.annotations.WorkbenchPerspective;
 import org.uberfire.mvp.impl.DefaultPlaceRequest;
+import org.uberfire.workbench.model.PanelDefinition;
 import org.uberfire.workbench.model.PanelType;
 import org.uberfire.workbench.model.PerspectiveDefinition;
+import org.uberfire.workbench.model.Position;
+import org.uberfire.workbench.model.impl.PanelDefinitionImpl;
 import org.uberfire.workbench.model.impl.PartDefinitionImpl;
 import org.uberfire.workbench.model.impl.PerspectiveDefinitionImpl;
 
@@ -50,6 +53,13 @@ public class DeploymentListPerspective {
         final PerspectiveDefinition p = new PerspectiveDefinitionImpl(PanelType.ROOT_LIST);
         p.setName("Deployments");
         p.getRoot().addPart(new PartDefinitionImpl(new DefaultPlaceRequest("Deployments List")));
+
+        final PanelDefinition problems = new PanelDefinitionImpl( PanelType.MULTI_LIST );
+        problems.setHeight(200);
+        problems.setMinWidth(100);
+        problems.addPart(new PartDefinitionImpl(new DefaultPlaceRequest("org.kie.guvnor.Problems")));
+        p.getRoot().insertChild( Position.SOUTH, problems );
+
         p.setTransient(true);
         return p;
     }
