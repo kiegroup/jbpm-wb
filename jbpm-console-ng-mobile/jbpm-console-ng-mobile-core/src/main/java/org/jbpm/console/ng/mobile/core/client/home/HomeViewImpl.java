a6875c4ccba396895ee4736c9dd5a3fea56ea81c
Tomáš Livora <livoratom@gmail.com>
2014-05-04 20:40:49 +0200

    Change appearance of Home screen

--- jbpm-console-ng-mobile/jbpm-console-ng-mobile-core/src/main/java/org/jbpm/console/ng/mobile/core/client/home/HomeViewImpl.java
+++ jbpm-console-ng-mobile/jbpm-console-ng-mobile-core/src/main/java/org/jbpm/console/ng/mobile/core/client/home/HomeViewImpl.java
@@ -15,17 +15,19 @@
  */
 package org.jbpm.console.ng.mobile.core.client.home;
 
-import com.google.gwt.user.client.ui.ScrollPanel;
-import com.googlecode.mgwt.dom.client.event.tap.TapEvent;
-import com.googlecode.mgwt.dom.client.event.tap.TapHandler;
 import com.googlecode.mgwt.mvp.client.Animation;
-import com.googlecode.mgwt.ui.client.widget.Button;
-import com.googlecode.mgwt.ui.client.widget.RoundPanel;
+import com.googlecode.mgwt.ui.client.widget.CellList;
+import com.googlecode.mgwt.ui.client.widget.ScrollPanel;
+import com.googlecode.mgwt.ui.client.widget.celllist.BasicCell;
+import com.googlecode.mgwt.ui.client.widget.celllist.CellSelectedEvent;
+import com.googlecode.mgwt.ui.client.widget.celllist.CellSelectedHandler;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import javax.enterprise.context.Dependent;
 import javax.inject.Inject;
-import org.jbpm.console.ng.mobile.core.client.MGWTPlaceManager;
 import org.jbpm.console.ng.mobile.core.client.AbstractView;
+import org.jbpm.console.ng.mobile.core.client.MGWTPlaceManager;
 
 /**
  *
@@ -34,11 +36,7 @@ import org.jbpm.console.ng.mobile.core.client.AbstractView;
 @Dependent
 public class HomeViewImpl extends AbstractView implements HomePresenter.HomeView {
 
-    private final Button processDefinitionsButton;
-
-    private final Button processInstancesButton;
-
-    private final Button tasksListButton;
+    private final CellList<String> cellList;
 
     private HomePresenter presenter;
 
@@ -49,47 +47,50 @@ public class HomeViewImpl extends AbstractView implements HomePresenter.HomeView
         title.setHTML("jBPM Mobile");
         headerBackButton.setVisible(false);
 
+        cellList = new CellList<String>(new BasicCell<String>() {
+            @Override
+            public String getDisplayString(String model) {
+                return model;
+            }
+        });
+        cellList.setRound(true);
+
         ScrollPanel scrollPanel = new ScrollPanel();
+        scrollPanel.setWidget(cellList);
+        scrollPanel.setScrollingEnabledX(false);
         layoutPanel.add(scrollPanel);
-
-        RoundPanel homePanel = new RoundPanel();
-
-        processDefinitionsButton = new Button("Process Definitions");
-        homePanel.add(processDefinitionsButton);
-
-        processInstancesButton = new Button("Process Instances");
-        homePanel.add(processInstancesButton);
-
-        tasksListButton = new Button("Tasks List");
-        homePanel.add(tasksListButton);
-
-        scrollPanel.add(homePanel);
     }
 
     @Override
     public void init(HomePresenter presenter) {
         this.presenter = presenter;
 
-        processDefinitionsButton.addTapHandler(new TapHandler() {
+        cellList.addCellSelectedHandler(new CellSelectedHandler() {
             @Override
-            public void onTap(TapEvent event) {
-                placeManager.goTo("Process Definitions List", Animation.SLIDE);
+            public void onCellSelected(CellSelectedEvent event) {
+                String nextScreen = null;
+                switch (event.getIndex()) {
+                    case 0:
+                        nextScreen = "Process Definitions List";
+                        break;
+                    case 1:
+                        nextScreen = "Process Instances List";
+                        break;
+                    case 2:
+                        nextScreen = "Tasks List";
+                        break;
+                    default:
+                        return;
+                }
+                placeManager.goTo(nextScreen, Animation.SLIDE, null);
             }
         });
 
-        processInstancesButton.addTapHandler(new TapHandler() {
-            @Override
-            public void onTap(TapEvent event) {
-                placeManager.goTo("Process Instances List", Animation.SLIDE, null);
-            }
-        });
-
-        tasksListButton.addTapHandler(new TapHandler() {
-            @Override
-            public void onTap(TapEvent event) {
-                placeManager.goTo("Tasks List", Animation.SLIDE);
-            }
-        });
+        List<String> sections = new ArrayList<String>();
+        sections.add("Process Definitions");
+        sections.add("Process Instances");
+        sections.add("Tasks List");
+        cellList.render(sections);
     }
 
     @Override
