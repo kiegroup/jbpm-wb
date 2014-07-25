530d4401ee68a19d5daad05dc5cafb555a76db10
David Gutierrez <dgutierr@redhat.com>
2014-07-25 11:16:21 +0200

    BZ-1100150 - Cannot open 'Process & Task Dashboard' under NAT environment

--- jbpm-console-ng-dashboard/jbpm-console-ng-dashboard-backend/src/main/java/org/jbpm/dashboard/renderer/service/impl/DashboardRendererServiceImpl.java
+++ jbpm-console-ng-dashboard/jbpm-console-ng-dashboard-backend/src/main/java/org/jbpm/dashboard/renderer/service/impl/DashboardRendererServiceImpl.java
@@ -7,23 +7,59 @@ import org.jbpm.dashboard.renderer.service.ConnectionStatus;
 import javax.enterprise.context.ApplicationScoped;
 
 import java.net.HttpURLConnection;
+import java.net.InetAddress;
 import java.net.URL;
+import java.util.List;
+import java.util.ArrayList;
 
 @Service
 @ApplicationScoped
 public class DashboardRendererServiceImpl implements DashboardRendererService {
 
     @Override
-    public ConnectionStatus getAppStatus(String anUrl) {
+    public ConnectionStatus getAppStatus(String theUrl) {
         ConnectionStatus connectionStatus = new ConnectionStatus();
-        try{
-            URL url = new URL(anUrl);
-            HttpURLConnection urlConnection = (HttpURLConnection)url.openConnection();
-            connectionStatus.setStatus(urlConnection.getResponseCode()) ;
-        }catch (Exception e){
-            e.printStackTrace();
-            connectionStatus.setStatus(-1);
+
+        // Get a list of the urls to check for the given url
+        List<String> urls = explodeUrl(theUrl);
+        Exception exc = null;
+        for (String anUrl : urls) {
+            try {
+                // Check whether the service is available
+                int status = pingUrl(anUrl);
+                connectionStatus.setStatus(status);
+                return connectionStatus;
+            } catch (Exception e){
+                exc = e;
+            }
         }
+        if (exc != null) exc.printStackTrace();
+        connectionStatus.setStatus(-1);
         return connectionStatus;
+    }
+
+    protected int pingUrl(String anUrl) throws Exception {
+        URL url = new URL(anUrl);
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        return urlConnection.getResponseCode();
+    }
+
+    protected List<String> explodeUrl(String anUrl) {
+        List<String> results = new ArrayList<String>();
+
+        // Add the target URL
+        results.add(anUrl);
+
+        // Add the "localhost" version
+        try {
+            String host = new URL(anUrl).getHost();
+            String localHost = InetAddress.getLocalHost().getHostAddress();
+            if (!host.equals(localHost)) {
+                results.add(anUrl.replace(host, localHost));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return results;
     }
 }
