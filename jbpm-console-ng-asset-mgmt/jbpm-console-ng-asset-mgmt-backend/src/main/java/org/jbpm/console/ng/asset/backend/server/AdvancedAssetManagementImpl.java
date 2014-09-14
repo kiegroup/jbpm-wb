e3be4c6f21074583b4f5fac6727f8ca1c176ec5b
Salaboy <salaboy@gmail.com>
2014-09-14 19:54:16 +0100

    - adding guvnor asset mgmt deps

--- jbpm-console-ng-asset-mgmt/jbpm-console-ng-asset-mgmt-backend/src/main/java/org/jbpm/console/ng/asset/backend/server/AdvancedAssetManagementImpl.java
+++ jbpm-console-ng-asset-mgmt/jbpm-console-ng-asset-mgmt-backend/src/main/java/org/jbpm/console/ng/asset/backend/server/AdvancedAssetManagementImpl.java
@@ -19,9 +19,9 @@ package org.jbpm.console.ng.asset.backend.server;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
-//import org.guvnor.asset.management.model.BuildProjectStructureEvent;
-//import org.guvnor.asset.management.model.ConfigureRepositoryEvent;
-//import org.guvnor.asset.management.model.PromoteChangesEvent;
+import org.guvnor.asset.management.model.BuildProjectStructureEvent;
+import org.guvnor.asset.management.model.ConfigureRepositoryEvent;
+import org.guvnor.asset.management.model.PromoteChangesEvent;
 
 import org.jbpm.console.ng.bd.service.KieSessionEntryPoint;
 
@@ -34,16 +34,16 @@ public class AdvancedAssetManagementImpl {
   public AdvancedAssetManagementImpl() {
   }
 
-//  public void configureRepository(@Observes ConfigureRepositoryEvent event) {
-//    sessionServices.startProcess("org.kie.management:asset-management-kmodule:1.0.0-SNAPSHOT", "asset-management-kmodule.ConfigureRepository", event.getParams());
-//  }
-//
-//  public void buildProject(@Observes BuildProjectStructureEvent event) {
-//    sessionServices.startProcess("org.kie.management:asset-management-kmodule:1.0.0-SNAPSHOT", "asset-management-kmodule.BuildProject", event.getParams());
-//  }
-//
-//  public void promoteChanges(@Observes PromoteChangesEvent event) {
-//    sessionServices.startProcess("org.kie.management:asset-management-kmodule:1.0.0-SNAPSHOT", "asset-management-kmodule.PromoteAssets", event.getParams());
-//  }
+  public void configureRepository(@Observes ConfigureRepositoryEvent event) {
+    sessionServices.startProcess("org.guvnor:guvnor-asset-mgmt-project:6.2.0-SNAPSHOT", "asset-management-kmodule.ConfigureRepository", event.getParams());
+  }
+
+  public void buildProject(@Observes BuildProjectStructureEvent event) {
+    sessionServices.startProcess("org.guvnor:guvnor-asset-mgmt-project:6.2.0-SNAPSHOT", "asset-management-kmodule.BuildProject", event.getParams());
+  }
+
+  public void promoteChanges(@Observes PromoteChangesEvent event) {
+    sessionServices.startProcess("org.guvnor:guvnor-asset-mgmt-project:6.2.0-SNAPSHOT", "asset-management-kmodule.PromoteAssets", event.getParams());
+  }
 
 }
