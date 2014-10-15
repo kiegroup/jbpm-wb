c65e321d351052ed495267edc0fda216bd516719
Jonathan Fuerth <jfuerth@gmail.com>
2014-10-15 14:32:11 -0400

    Debug logging to help diagnose the blank custom panel issue (not part of the fix)

--- jbpm-console-ng-human-tasks-forms/jbpm-console-ng-human-tasks-forms-client/src/main/java/org/jbpm/console/ng/ht/forms/client/editors/taskform/displayers/util/PlaceManagerFormActivitySearcher.java
+++ jbpm-console-ng-human-tasks-forms/jbpm-console-ng-human-tasks-forms-client/src/main/java/org/jbpm/console/ng/ht/forms/client/editors/taskform/displayers/util/PlaceManagerFormActivitySearcher.java
@@ -15,21 +15,29 @@
  */
 package org.jbpm.console.ng.ht.forms.client.editors.taskform.displayers.util;
 
-import com.google.gwt.user.client.ui.HasWidgets;
+import java.util.HashMap;
+import java.util.IdentityHashMap;
 import java.util.Map;
+
 import javax.enterprise.context.Dependent;
 import javax.inject.Inject;
 
-import java.util.HashMap;
 import org.uberfire.client.mvp.PlaceManager;
 import org.uberfire.client.mvp.PlaceStatus;
+import org.uberfire.debug.Debug;
 import org.uberfire.mvp.impl.DefaultPlaceRequest;
+
+import com.google.gwt.event.logical.shared.AttachEvent;
+import com.google.gwt.user.client.ui.HasWidgets;
+import com.google.gwt.user.client.ui.Panel;
 
 /**
  * @author pefernan
  */
 @Dependent
 public class PlaceManagerFormActivitySearcher {
+
+    private static IdentityHashMap<Panel, Integer> customContainers = new IdentityHashMap<Panel, Integer>();
 
     @Inject
     private PlaceManager placeManager;
@@ -42,6 +50,22 @@ public class PlaceManagerFormActivitySearcher {
             params = new HashMap<String, String>();
         }
         DefaultPlaceRequest defaultPlaceRequest = new DefaultPlaceRequest(name + " Form", params);
+
+        final Panel container = (Panel) widget;
+        if ( customContainers.get( container ) == null ) {
+
+            System.out.println("Got new custom container " + Debug.objectId( widget ));
+            container.getElement().addClassName( "custom-container-" + Debug.toMemorableString( System.identityHashCode( widget ) ) + "-" + customContainers.size() );
+            customContainers.put( container, customContainers.size() );
+            container.addAttachHandler( new AttachEvent.Handler() {
+                @Override
+                public void onAttachOrDetach( AttachEvent event ) {
+                    new Exception( "Container attached " + event.isAttached() + ": " + container.getElement().getId()).printStackTrace(System.out);
+                }
+            } );
+        } else {
+            System.out.println("Reusing custom container " + container.getElement().getAttribute( "class" ));
+        }
         widget.clear();
         PlaceStatus status = placeManager.getStatus(defaultPlaceRequest);
         if(status.equals(PlaceStatus.OPEN)){
