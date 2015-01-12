694961faeb2fbb79ad57dfd63dd85603887f703f
xiabai <xiabai@dhcp-137-101.nay.redhat.com>
2015-01-12 14:47:12 +0800

    BZ-1165089 - [RFE]process initiator（or process admin） can not check process instance history details                  1 add usertask log tab page

--- jbpm-console-ng-process-runtime/jbpm-console-ng-process-runtime-api/src/main/java/org/jbpm/console/ng/pr/model/UserTaskSummary.java
+++ jbpm-console-ng-process-runtime/jbpm-console-ng-process-runtime-api/src/main/java/org/jbpm/console/ng/pr/model/UserTaskSummary.java
@@ -1,15 +1,22 @@
 package org.jbpm.console.ng.pr.model;
 
+import java.util.Date;
+
 import org.jboss.errai.common.client.api.annotations.Portable;
+import org.jbpm.console.ng.ga.model.GenericSummary;
 
 @Portable
-public class UserTaskSummary {
+public class UserTaskSummary extends GenericSummary {
 
-    private Long id;
+	private Long id;
     private String name;
     private String owner;
     private String status;
-
+    private String description;
+    private String createdBy;
+    private Date createdOn;
+    private Date activationTime;
+    private Date expirationTime;
     public UserTaskSummary() {
 
     }
@@ -24,8 +31,25 @@ public class UserTaskSummary {
             this.owner = "---";
         }
     }
+    
+    
 
-    public Long getId() {
+    public UserTaskSummary(Long id, String name, String owner, String status,
+			String description, String createdBy,
+			Date createdOn, Date activationTime, Date expirationTime) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.owner = owner;
+		this.status = status;
+		this.description = description;
+		this.createdBy = createdBy;
+		this.createdOn = createdOn;
+		this.activationTime = activationTime;
+		this.expirationTime = expirationTime;
+	}
+
+	public Long getId() {
         return id;
     }
 
@@ -56,4 +80,45 @@ public class UserTaskSummary {
     public void setStatus(String status) {
         this.status = status;
     }
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public Date getCreatedOn() {
+		return createdOn;
+	}
+
+	public void setCreatedOn(Date createdOn) {
+		this.createdOn = createdOn;
+	}
+
+	public Date getActivationTime() {
+		return activationTime;
+	}
+
+	public void setActivationTime(Date activationTime) {
+		this.activationTime = activationTime;
+	}
+
+	public Date getExpirationTime() {
+		return expirationTime;
+	}
+
+	public void setExpirationTime(Date expirationTime) {
+		this.expirationTime = expirationTime;
+	}
+    
 }
