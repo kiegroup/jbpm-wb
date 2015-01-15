76413cd9c871647ded976c7f02581dd397bd7cef
Maciej Swiderski <mswiders@redhat.com>
2015-01-15 11:50:12 +0100

    BZ-1165287 - Bulk signal for all process instances does not trigger them in Business Central nor remotely via REST or JMS - disabled bulk operations when no instances are selected

--- jbpm-console-ng-process-runtime/jbpm-console-ng-process-runtime-client/src/main/java/org/jbpm/console/ng/pr/client/editors/instance/list/ProcessInstanceListViewImpl.java
+++ jbpm-console-ng-process-runtime/jbpm-console-ng-process-runtime-client/src/main/java/org/jbpm/console/ng/pr/client/editors/instance/list/ProcessInstanceListViewImpl.java
@@ -96,6 +96,19 @@ public class ProcessInstanceListViewImpl extends AbstractListView<ProcessInstanc
 
     private Column actionsColumn;
 
+    private NavLink bulkAbortNavLink;
+    private NavLink bulkSignalNavLink;
+
+    private void controlBulkOperations() {
+        if (selectedProcessInstances != null && selectedProcessInstances.size() > 0) {
+            bulkAbortNavLink.setDisabled(false);
+            bulkSignalNavLink.setDisabled(false);
+        } else {
+            bulkAbortNavLink.setDisabled(true);
+            bulkSignalNavLink.setDisabled(true);
+        }
+    }
+
     @Override
     public void init(final ProcessInstanceListPresenter presenter) {
         List<String> bannedColumns = new ArrayList<String>();
@@ -187,6 +200,7 @@ public class ProcessInstanceListViewImpl extends AbstractListView<ProcessInstanc
                                         selectedProcessInstances.remove(event.getValue());
                                         input.setChecked(false);
                                     }
+                                    controlBulkOperations();
                                     return DefaultSelectionEventManager.SelectAction.IGNORE;
                                 }
                             }
@@ -229,7 +243,7 @@ public class ProcessInstanceListViewImpl extends AbstractListView<ProcessInstanc
     private void initBulkActionsDropDown() {
         SplitDropdownButton bulkActions = new SplitDropdownButton();
         bulkActions.setText(constants.Bulk_Actions());
-        NavLink bulkAbortNavLink = new NavLink(constants.Bulk_Abort());
+        bulkAbortNavLink = new NavLink(constants.Bulk_Abort());
         bulkAbortNavLink.setIcon(IconType.REMOVE_SIGN);
         bulkAbortNavLink.addClickHandler(new ClickHandler() {
             @Override
@@ -240,7 +254,7 @@ public class ProcessInstanceListViewImpl extends AbstractListView<ProcessInstanc
             }
         });
 
-        NavLink bulkSignalNavLink = new NavLink(constants.Bulk_Signal());
+        bulkSignalNavLink = new NavLink(constants.Bulk_Signal());
         bulkSignalNavLink.setIcon(IconType.BELL);
         bulkSignalNavLink.addClickHandler(new ClickHandler() {
             @Override
@@ -254,6 +268,8 @@ public class ProcessInstanceListViewImpl extends AbstractListView<ProcessInstanc
         bulkActions.add(bulkAbortNavLink);
         bulkActions.add(bulkSignalNavLink);
         listGrid.getLeftToolbar().add(bulkActions);
+
+        controlBulkOperations();
     }
 
     private void initFiltersBar() {
