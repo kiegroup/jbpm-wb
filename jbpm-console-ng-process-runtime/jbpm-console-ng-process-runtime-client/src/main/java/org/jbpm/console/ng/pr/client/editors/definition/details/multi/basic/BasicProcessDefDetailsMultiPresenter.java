5001b800f98eb4ea8d6cf864ed930039656eb78d
xiabai <xiabai@dhcp-136-29.nay.redhat.com>
2015-03-24 15:45:29 +0800

    REV: mutiple view  mode of process definitions  and refacting for baisc and advanced mode      and some formatter issue fix

---
+++ jbpm-console-ng-process-runtime/jbpm-console-ng-process-runtime-client/src/main/java/org/jbpm/console/ng/pr/client/editors/definition/details/multi/basic/BasicProcessDefDetailsMultiPresenter.java
@@ -0,0 +1,103 @@
+package org.jbpm.console.ng.pr.client.editors.definition.details.multi.basic;
+
+import javax.enterprise.context.Dependent;
+import javax.inject.Inject;
+import org.jbpm.console.ng.pr.client.editors.definition.details.multi.BaseProcessDefDetailsMultiPresenter;
+import org.uberfire.client.annotations.DefaultPosition;
+import org.uberfire.client.annotations.WorkbenchMenu;
+import org.uberfire.client.annotations.WorkbenchPartView;
+import org.uberfire.client.annotations.WorkbenchScreen;
+import org.uberfire.client.mvp.UberView;
+import org.uberfire.workbench.model.Position;
+import org.uberfire.workbench.model.menu.MenuFactory;
+import org.uberfire.workbench.model.menu.MenuItem;
+import org.uberfire.workbench.model.menu.Menus;
+import org.uberfire.workbench.model.menu.impl.BaseMenuCustom;
+import com.google.gwt.user.client.ui.IsWidget;
+
+@Dependent
+@WorkbenchScreen(identifier = "Basic Process Details Multi", preferredWidth = 500)
+public class BasicProcessDefDetailsMultiPresenter extends
+        BaseProcessDefDetailsMultiPresenter {
+
+    public interface BasicProcessDefDetailsMultiView extends
+            BaseProcessDefDetailsMultiPresenter.BaseProcessDefDetailsMultiView {
+
+    }
+
+    @Inject
+    BasicProcessDefDetailsMultiView view;
+
+    @DefaultPosition
+    public Position getPosition() {
+        return super.getDefaultPosition();
+    }
+
+    @WorkbenchPartView
+    public UberView<BaseProcessDefDetailsMultiPresenter> getView() {
+        return view;
+    }
+
+    @Override
+    protected void setDefaultTab() {
+        view.getTabPanel().selectTab( 0 );
+    }
+
+    @WorkbenchMenu
+    public Menus buildMenu() {
+        return MenuFactory
+                .newTopLevelCustomMenu( new MenuFactory.CustomMenuBuilder() {
+
+                    @Override
+                    public void push( MenuFactory.CustomMenuBuilder element ) {
+                    }
+
+                    @Override
+                    public MenuItem build() {
+                        return new BaseMenuCustom<IsWidget>() {
+
+                            @Override
+                            public IsWidget build() {
+                                return view.getNewInstanceButton();
+                            }
+                        };
+                    }
+                } ).endMenu()
+                
+                .newTopLevelCustomMenu( new MenuFactory.CustomMenuBuilder() {
+
+                    @Override
+                    public void push( MenuFactory.CustomMenuBuilder element ) {
+                    }
+
+                    @Override
+                    public MenuItem build() {
+                        return new BaseMenuCustom<IsWidget>() {
+
+                            @Override
+                            public IsWidget build() {
+                                return view.getRefreshButton();
+                            }
+                        };
+                    }
+                } ).endMenu()
+
+                .newTopLevelCustomMenu( new MenuFactory.CustomMenuBuilder() {
+
+                    @Override
+                    public void push( MenuFactory.CustomMenuBuilder element ) {
+                    }
+
+                    @Override
+                    public MenuItem build() {
+                        return new BaseMenuCustom<IsWidget>() {
+
+                            @Override
+                            public IsWidget build() {
+                                return view.getCloseButton();
+                            }
+                        };
+                    }
+                } ).endMenu().build();
+    }
+}
---
+++ jbpm-console-ng-process-runtime/jbpm-console-ng-process-runtime-client/src/main/java/org/jbpm/console/ng/pr/client/editors/definition/details/multi/basic/BasicProcessDefDetailsMultiViewImpl.java
@@ -0,0 +1,44 @@
+package org.jbpm.console.ng.pr.client.editors.definition.details.multi.basic;
+
+import javax.enterprise.context.Dependent;
+import javax.inject.Inject;
+import org.jbpm.console.ng.pr.client.editors.definition.details.BaseProcessDefDetailsPresenter;
+import org.jbpm.console.ng.pr.client.editors.definition.details.basic.BasicProcessDefDetailsPresenter;
+import org.jbpm.console.ng.pr.client.editors.definition.details.multi.BaseProcessDefDetailsMultiViewImpl;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.user.client.ui.Widget;
+
+@Dependent
+public class BasicProcessDefDetailsMultiViewImpl extends
+        BaseProcessDefDetailsMultiViewImpl implements
+        BasicProcessDefDetailsMultiPresenter.BasicProcessDefDetailsMultiView {
+
+    interface Binder
+            extends
+            UiBinder<Widget, BasicProcessDefDetailsMultiPresenter.BasicProcessDefDetailsMultiView> {
+
+    }
+
+    private static Binder uiBinder = GWT.create( Binder.class );
+    
+    @Inject
+    private BasicProcessDefDetailsPresenter detailsPresenter;
+
+    @Override
+    protected BaseProcessDefDetailsPresenter getSpecificProcessDefDetailPresenter() {
+        return detailsPresenter;
+    }
+
+    @Override
+    protected void createAndBindUi() {
+        uiBinder.createAndBindUi( this );
+
+    }
+
+    @Override
+    protected int getSpecificOffsetHeight() {
+        return BasicProcessDefDetailsMultiViewImpl.this.getParent()
+                .getOffsetHeight();
+    }
+}
