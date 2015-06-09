33d58be6efbd8997b841f4115bbc62f6d8c7ce5f
salaboy <salaboy@gmail.com>
2015-06-09 12:49:48 +0100

    BPMSPL-141: [GSS-RFE] fix for the JIRA changes

--- jbpm-console-ng-asset-mgmt/jbpm-console-ng-asset-mgmt-backend/src/test/java/org/jbpm/console/ng/asset/backend/server/AssetMgmtDeploymentUnitProviderTest.java
+++ jbpm-console-ng-asset-mgmt/jbpm-console-ng-asset-mgmt-backend/src/test/java/org/jbpm/console/ng/asset/backend/server/AssetMgmtDeploymentUnitProviderTest.java
@@ -2,11 +2,13 @@ package org.jbpm.console.ng.asset.backend.server;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 import java.util.Set;
 
+import org.guvnor.common.services.project.model.GAV;
 import org.guvnor.m2repo.backend.server.GuvnorM2Repository;
 import org.jbpm.services.api.model.DeploymentUnit;
 import org.junit.Test;
@@ -18,23 +20,23 @@ public class AssetMgmtDeploymentUnitProviderTest {
     /**
      * This test makes sure that the {@link AssetMgmtDeploymentUnitProvider#getDeploymentUnits()}
      * does what it should.
-     * 
+     *
      * @throws Exception If something goes wrong
      */
     @Test
-    public void testGetDeploymentUnits() throws Exception { 
+    public void testGetDeploymentUnits() throws Exception {
        AssetMgmtDeploymentUnitProvider provider = new AssetMgmtDeploymentUnitProvider();
-      
+
        // setup provider with spy
        GuvnorM2Repository repo = new GuvnorM2Repository();
        repo.init();
        GuvnorM2Repository repoSpy = Mockito.spy(repo);
-       provider.setM2Repository(repoSpy); 
-   
+       provider.setM2Repository(repoSpy);
+
        // Create argument captor
-       ArgumentCaptor<String> fileNameCaptor = 
-               ArgumentCaptor.forClass(String.class);
-       
+       ArgumentCaptor<GAV> fileNameCaptor =
+               ArgumentCaptor.forClass(GAV.class);
+
        // run method
        Set<DeploymentUnit> units = provider.getDeploymentUnits();
 
@@ -43,14 +45,14 @@ public class AssetMgmtDeploymentUnitProviderTest {
        // When the a method is called on the spy, it automatically stores the arg values
        // -- you just need to add a captor.capture() to the verify statement
        //    to retrieve the value later
-       verify(repoSpy, times(1)).getFileName(fileNameCaptor.capture());
+       verify(repoSpy, times(1)).getArtifactFileFromRepository(fileNameCaptor.capture());
 
-       // Was the guvnorm2Repository.getFileName(..) method called 
+       // Was the guvnorm2Repository.getFileName(..) method called
        //  when we called provider.getDeploymentUnits() ?
-       String fileNameArg = fileNameCaptor.getValue();
-       assertTrue( "Incorrect file name: " + fileNameArg, 
-               fileNameArg != null && fileNameArg.contains("guvnor-asset-mgmt-project"));
-       
+       GAV fileNameArg = fileNameCaptor.getValue();
+       assertTrue( "Incorrect file name: " + fileNameArg,
+               fileNameArg != null && fileNameArg.getArtifactId().equals("guvnor-asset-mgmt-project"));
+
        // verify that the return value is not empty
        assertNotNull( "Null deployment units set", units);
        assertNotNull( "Empty deployment units set", units.isEmpty());
