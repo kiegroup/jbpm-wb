2fd9f6547a132d47be9b36c6076de504d91a078f
nmirasch <neus.miras@gmail.com>
2015-10-13 17:56:37 +0200

    BZ-1270690 - Task list: Active Tab shows completed tasks

--- jbpm-console-ng-process-runtime/jbpm-console-ng-process-runtime-client/src/main/java/org/jbpm/console/ng/pr/client/editors/instance/list/variables/dash/DataSetProcessInstanceWithVariablesListViewImpl.java
+++ jbpm-console-ng-process-runtime/jbpm-console-ng-process-runtime-client/src/main/java/org/jbpm/console/ng/pr/client/editors/instance/list/variables/dash/DataSetProcessInstanceWithVariablesListViewImpl.java
@@ -921,6 +921,7 @@ public class DataSetProcessInstanceWithVariablesListViewImpl extends AbstractMul
         ArrayList<String> existingGrids = getMultiGridPreferencesStore().getGridsId();
         ArrayList<String> allTabs = new ArrayList<String>(existingGrids.size());
 
+        presenter.setAddingDefaultFilters( true );
         if (existingGrids != null && existingGrids.size() > 0) {
 
             for (int i = 0; i < existingGrids.size(); i++) {
