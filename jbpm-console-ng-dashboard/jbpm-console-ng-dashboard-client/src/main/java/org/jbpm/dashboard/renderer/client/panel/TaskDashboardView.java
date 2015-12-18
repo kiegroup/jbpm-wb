6d906c8df713eb44b7e7d144f63fc8d95e572f83
Michael Biarnes Kiefer <mbiarnes@redhat.com>
2015-12-18 16:51:06 +0100

    changes in license header

--- jbpm-console-ng-dashboard/jbpm-console-ng-dashboard-client/src/main/java/org/jbpm/dashboard/renderer/client/panel/TaskDashboardView.java
+++ jbpm-console-ng-dashboard/jbpm-console-ng-dashboard-client/src/main/java/org/jbpm/dashboard/renderer/client/panel/TaskDashboardView.java
@@ -1,5 +1,6 @@
 /**
- * Copyright (C) 2015 JBoss Inc
+ * Copyright (C) 2015 Red Hat, Inc. and/or its affiliates.
+
  * <p/>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
