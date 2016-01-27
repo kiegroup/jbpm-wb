29d1bbec1144b0097a4c314f45ffa4d7147b15a3
nmirasch <neus.miras@gmail.com>
2016-01-27 21:51:48 +0100

    JBPM-4718: Rename Complete button in tasklist to make it reflect its true function

--- jbpm-console-ng-human-tasks/jbpm-console-ng-human-tasks-client/src/main/java/org/jbpm/console/ng/ht/client/editors/taskslist/grid/dash/DataSetTasksListGridViewImpl.java
+++ jbpm-console-ng-human-tasks/jbpm-console-ng-human-tasks-client/src/main/java/org/jbpm/console/ng/ht/client/editors/taskslist/grid/dash/DataSetTasksListGridViewImpl.java
@@ -476,7 +476,7 @@ public class DataSetTasksListGridViewImpl extends AbstractMultiGridView<TaskSumm
             }
         } ) );
 
-        cells.add( new CompleteActionHasCell( constants.Complete(), new ActionCell.Delegate<TaskSummary>() {
+        cells.add( new CompleteActionHasCell( constants.Open(), new ActionCell.Delegate<TaskSummary>() {
             @Override
             public void execute( TaskSummary task ) {
                 placeManager.goTo( "Task Details Multi" );
@@ -484,6 +484,7 @@ public class DataSetTasksListGridViewImpl extends AbstractMultiGridView<TaskSumm
                 if ( task.getStatus().equals( "Completed" ) && task.isLogOnly() ) {
                     logOnly = true;
                 }
+                selectedRow=-1;
                 taskSelected.fire( new TaskSelectionEvent( task.getTaskId(), task.getName(), task.isForAdmin(), logOnly ) );
             }
         } ) );
