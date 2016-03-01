883780c203ad9dcc991f5ec37a2039f82df981b2
nmirasch <neus.miras@gmail.com>
2016-03-01 10:51:09 +0100

    BZ1174807: JobExecutor: Job Name column is not displayed

--- jbpm-console-ng-executor-service/jbpm-console-ng-executor-service-api/src/main/java/org/jbpm/console/ng/es/model/RequestDataSetConstants.java
+++ jbpm-console-ng-executor-service/jbpm-console-ng-executor-service-api/src/main/java/org/jbpm/console/ng/es/model/RequestDataSetConstants.java
@@ -25,4 +25,5 @@ public interface RequestDataSetConstants {
     String COLUMN_COMMANDNAME = "commandName";
     String COLUMN_MESSAGE = "message";
     String COLUMN_BUSINESSKEY = "businessKey";
+    String COLUMN_RETRIES= "retries";
 }
