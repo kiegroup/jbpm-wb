cb1fcaee50013ffc2df375daee1080b9396942bb
nmirasch <neus.miras@gmail.com>
2016-03-04 16:03:34 +0100

    JBPM-4767: 2 Process/Task dashboard details screen (in log mode) switch to default after refresh. Added Process Instance Details in log mode

--- jbpm-console-ng-process-runtime/jbpm-console-ng-process-runtime-api/src/main/java/org/jbpm/console/ng/pr/model/events/ProcessInstanceSelectionEvent.java
+++ jbpm-console-ng-process-runtime/jbpm-console-ng-process-runtime-api/src/main/java/org/jbpm/console/ng/pr/model/events/ProcessInstanceSelectionEvent.java
@@ -20,84 +20,96 @@ import org.jboss.errai.common.client.api.annotations.Portable;
 @Portable
 public class ProcessInstanceSelectionEvent {
 
-  private Long processInstanceId;
-  private String processDefId;
-  private String deploymentId;
-  private Integer processInstanceStatus;
-  private String processDefName;
+    private Long processInstanceId;
+    private String processDefId;
+    private String deploymentId;
+    private Integer processInstanceStatus;
+    private String processDefName;
+    private boolean forLog;
 
-  public ProcessInstanceSelectionEvent() {
-  }
-
-  public ProcessInstanceSelectionEvent(String deploymentId, Long processInstanceId, String processDefId, String processDefName, Integer processInstanceStatus) {
-    this.processInstanceId = processInstanceId;
-    this.processDefId = processDefId;
-    this.deploymentId = deploymentId;
-    this.processInstanceStatus = processInstanceStatus;
-    this.processDefName = processDefName;
-  }
-
-  public Long getProcessInstanceId() {
-    return processInstanceId;
-  }
-
-  public String getProcessDefId() {
-    return processDefId;
-  }
-
-  public String getDeploymentId() {
-    return deploymentId;
-  }
-
-  public Integer getProcessInstanceStatus() {
-    return processInstanceStatus;
-  }
-
-  public String getProcessDefName() {
-    return processDefName;
-  }
-
-  @Override
-  public String toString() {
-    return "ProcessInstanceSelectionEvent{" + "processInstanceId=" + processInstanceId + ", processDefId=" + processDefId + ", deploymentId=" + deploymentId + ", processInstanceStatus=" + processInstanceStatus + ", processDefName=" + processDefName + '}';
-  }
-
-  @Override
-  public int hashCode() {
-    int hash = 7;
-    hash = 37 * hash + (this.processInstanceId != null ? this.processInstanceId.hashCode() : 0);
-    hash = 37 * hash + (this.processDefId != null ? this.processDefId.hashCode() : 0);
-    hash = 37 * hash + (this.deploymentId != null ? this.deploymentId.hashCode() : 0);
-    hash = 37 * hash + (this.processInstanceStatus != null ? this.processInstanceStatus.hashCode() : 0);
-    hash = 37 * hash + (this.processDefName != null ? this.processDefName.hashCode() : 0);
-    return hash;
-  }
-
-  @Override
-  public boolean equals(Object obj) {
-    if (obj == null) {
-      return false;
+    public ProcessInstanceSelectionEvent() {
     }
-    if (getClass() != obj.getClass()) {
-      return false;
+
+    public ProcessInstanceSelectionEvent(String deploymentId, Long processInstanceId, String processDefId, String processDefName, Integer processInstanceStatus) {
+        this.processInstanceId = processInstanceId;
+        this.processDefId = processDefId;
+        this.deploymentId = deploymentId;
+        this.processInstanceStatus = processInstanceStatus;
+        this.processDefName = processDefName;
+        this.forLog = false;
     }
-    final ProcessInstanceSelectionEvent other = (ProcessInstanceSelectionEvent) obj;
-    if (this.processInstanceId != other.processInstanceId && (this.processInstanceId == null || !this.processInstanceId.equals(other.processInstanceId))) {
-      return false;
+
+    public ProcessInstanceSelectionEvent(String deploymentId, Long processInstanceId, String processDefId,
+                                         String processDefName, Integer processInstanceStatus, boolean forLog) {
+        this(deploymentId, processInstanceId, processDefId, processDefName, processInstanceStatus);
+        this.forLog = forLog;
     }
-    if ((this.processDefId == null) ? (other.processDefId != null) : !this.processDefId.equals(other.processDefId)) {
-      return false;
+
+    public Long getProcessInstanceId() {
+        return processInstanceId;
     }
-    if ((this.deploymentId == null) ? (other.deploymentId != null) : !this.deploymentId.equals(other.deploymentId)) {
-      return false;
+
+    public String getProcessDefId() {
+        return processDefId;
     }
-    if (this.processInstanceStatus != other.processInstanceStatus && (this.processInstanceStatus == null || !this.processInstanceStatus.equals(other.processInstanceStatus))) {
-      return false;
+
+    public String getDeploymentId() {
+        return deploymentId;
     }
-    if ((this.processDefName == null) ? (other.processDefName != null) : !this.processDefName.equals(other.processDefName)) {
-      return false;
+
+    public Integer getProcessInstanceStatus() {
+        return processInstanceStatus;
     }
-    return true;
-  }
+
+    public String getProcessDefName() {
+        return processDefName;
+    }
+
+    @Override
+    public String toString() {
+        return "ProcessInstanceSelectionEvent{" + "processInstanceId=" + processInstanceId + ", processDefId=" + processDefId + ", deploymentId=" + deploymentId + ", processInstanceStatus=" + processInstanceStatus + ", processDefName=" + processDefName + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 37 * hash + (this.processInstanceId != null ? this.processInstanceId.hashCode() : 0);
+        hash = 37 * hash + (this.processDefId != null ? this.processDefId.hashCode() : 0);
+        hash = 37 * hash + (this.deploymentId != null ? this.deploymentId.hashCode() : 0);
+        hash = 37 * hash + (this.processInstanceStatus != null ? this.processInstanceStatus.hashCode() : 0);
+        hash = 37 * hash + (this.processDefName != null ? this.processDefName.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final ProcessInstanceSelectionEvent other = (ProcessInstanceSelectionEvent) obj;
+        if (this.processInstanceId != other.processInstanceId && (this.processInstanceId == null || !this.processInstanceId.equals(other.processInstanceId))) {
+            return false;
+        }
+        if ((this.processDefId == null) ? (other.processDefId != null) : !this.processDefId.equals(other.processDefId)) {
+            return false;
+        }
+        if ((this.deploymentId == null) ? (other.deploymentId != null) : !this.deploymentId.equals(other.deploymentId)) {
+            return false;
+        }
+        if (this.processInstanceStatus != other.processInstanceStatus && (this.processInstanceStatus == null || !this.processInstanceStatus.equals(other.processInstanceStatus))) {
+            return false;
+        }
+        if ((this.processDefName == null) ? (other.processDefName != null) : !this.processDefName.equals(other.processDefName)) {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean isForLog() {
+        return forLog;
+    }
 
 }
