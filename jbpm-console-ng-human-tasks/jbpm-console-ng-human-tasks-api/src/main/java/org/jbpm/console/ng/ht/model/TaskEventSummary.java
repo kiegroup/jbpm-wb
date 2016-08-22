ddd6fd427905c79e65b1ccf7bc3a8edd364354d3
Cristiano Nicolai <cristiano.nicolai@gmail.com>
2016-08-22 14:37:03 +1000

    JBPM-5274 - Propagate task event message

--- jbpm-console-ng-human-tasks/jbpm-console-ng-human-tasks-api/src/main/java/org/jbpm/console/ng/ht/model/TaskEventSummary.java
+++ jbpm-console-ng-human-tasks/jbpm-console-ng-human-tasks-api/src/main/java/org/jbpm/console/ng/ht/model/TaskEventSummary.java
@@ -16,70 +16,74 @@
 package org.jbpm.console.ng.ht.model;
 
 import java.util.Date;
+
 import org.jboss.errai.common.client.api.annotations.Portable;
 import org.jbpm.console.ng.ga.model.GenericSummary;
 
-/**
- *
- * @author salaboy
- */
 @Portable
 public class TaskEventSummary extends GenericSummary {
 
-  private Long eventId;
-  private Long taskId;
-  private String type;
-  private String userId;
-  private Date logTime;
-  private Long workItemId;
-  private String message;
+    private Long eventId;
+    private Long taskId;
+    private String type;
+    private String userId;
+    private Date logTime;
+    private Long workItemId;
+    private String message;
 
-  public TaskEventSummary() {
-  }
+    public TaskEventSummary() {
+    }
 
-  public TaskEventSummary(Long eventId, Long taskId, String type, String userId, Long workItemId, Date logTime, String message) {
-    this.id = eventId;
-    this.name = taskId + type;
-    this.eventId = eventId;
-    this.taskId = taskId;
-    this.type = type;
-    this.userId = userId;
-    this.logTime = logTime;
-    this.workItemId = workItemId;
-    this.message = message;
-  }
+    public TaskEventSummary(Long eventId, Long taskId, String type, String userId, Long workItemId, Date logTime, String message) {
+        this.id = eventId;
+        this.name = taskId + type;
+        this.eventId = eventId;
+        this.taskId = taskId;
+        this.type = type;
+        this.userId = userId;
+        this.logTime = logTime;
+        this.workItemId = workItemId;
+        this.message = message;
+    }
 
-  public Long getEventId() {
-    return eventId;
-  }
-  
-  public Long getTaskId() {
-    return taskId;
-  }
+    public Long getEventId() {
+        return eventId;
+    }
 
-  public String getType() {
-    return type;
-  }
+    public Long getTaskId() {
+        return taskId;
+    }
 
-  public String getUserId() {
-    return userId;
-  }
+    public String getType() {
+        return type;
+    }
 
-  public Date getLogTime() {
-    return logTime;
-  }
+    public String getUserId() {
+        return userId;
+    }
 
-  public Long getWorkItemId() {
-    return workItemId;
-  }
+    public Date getLogTime() {
+        return logTime;
+    }
 
-  public String getMessage() {
-    return message;
-  }
+    public Long getWorkItemId() {
+        return workItemId;
+    }
 
-  @Override
-  public String toString() {
-    return "TaskEventSummary{" + "eventId=" + eventId + ", taskId=" + taskId + ", type=" + type + ", userId=" + userId + ", logTime=" + logTime + ", workItemId=" + workItemId + '}';
-  }
+    public String getMessage() {
+        return message;
+    }
 
+    @Override
+    public String toString() {
+        return "TaskEventSummary{" +
+                "eventId=" + eventId +
+                ", taskId=" + taskId +
+                ", type='" + type + '\'' +
+                ", userId='" + userId + '\'' +
+                ", logTime=" + logTime +
+                ", workItemId=" + workItemId +
+                ", message='" + message + '\'' +
+                '}';
+    }
 }
