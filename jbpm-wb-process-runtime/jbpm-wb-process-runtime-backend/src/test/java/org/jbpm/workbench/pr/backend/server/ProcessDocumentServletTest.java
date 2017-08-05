a83683126d189a3d0e0e6684ce5fcc7a24e5dc9c
Tihomir Surdilovic <tsurdilovic@tihomirs-mbp.attlocal.net>
2017-08-05 13:48:11 -0700

    JBPM-6009 - Downloading a document from a kie-server requires (another) login

--- jbpm-wb-process-runtime/jbpm-wb-process-runtime-backend/src/test/java/org/jbpm/workbench/pr/backend/server/ProcessDocumentServletTest.java
+++ jbpm-wb-process-runtime/jbpm-wb-process-runtime-backend/src/test/java/org/jbpm/workbench/pr/backend/server/ProcessDocumentServletTest.java
@@ -15,16 +15,22 @@
  */
 package org.jbpm.workbench.pr.backend.server;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.text.MessageFormat;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import javax.enterprise.event.Event;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.WriteListener;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.spi.LoggingEvent;
+import ch.qos.logback.core.Appender;
 import org.jbpm.workbench.ks.integration.KieServerIntegration;
-import org.jbpm.workbench.pr.backend.server.org.jbpm.workbench.pr.backend.server.helper.TestHttpServletRequest;
-import org.jbpm.workbench.pr.backend.server.org.jbpm.workbench.pr.backend.server.helper.TestHttpServletResponse;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -35,18 +41,13 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
-import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.uberfire.mocks.EventSourceMock;
-import org.uberfire.workbench.events.NotificationEvent;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ProcessDocumentServletTest {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(ProcessDocumentServletTest.class);
 
     @Mock
     KieServerIntegration kieServerIntegration;
@@ -60,74 +61,74 @@ public class ProcessDocumentServletTest {
     @Mock
     DocumentInstance documentInstance;
 
-    @Captor
-    protected ArgumentCaptor<NotificationEvent> notificationCaptor;
-
-    protected Event<NotificationEvent> notification = mock(EventSourceMock.class);
-
-    protected final List<Object> receivedNotificationEvents = new ArrayList<Object>();
-
-    protected Event<NotificationEvent> notificationEvents = new EventSourceMock<NotificationEvent>() {
-        @Override
-        public void fire(NotificationEvent event) {
-            receivedNotificationEvents.add(event);
-        }
-    };
-
     Map<String, String> params;
+
+    @Mock
+    private Appender loggingAppender;
+
+    @Captor
+    private ArgumentCaptor<LoggingEvent> loggingEventArgumentCaptor;
+
+    private ProcessDocumentServlet processDocServlet;
 
     @Before
     public void setup() {
         params = new HashMap();
+        processDocServlet = new ProcessDocumentServlet();
+
+        final Logger logger = (Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
+        logger.addAppender(loggingAppender);
+        logger.setLevel(Level.INFO);
+    }
+
+    @After
+    public void teardown() {
+        final Logger logger = (Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
+        logger.detachAppender(loggingAppender);
     }
 
     @Test
     public void testInvalidRequestParamets() throws Exception {
-        TestHttpServletRequest request = new TestHttpServletRequest(params);
-        TestHttpServletResponse response = new TestHttpServletResponse();
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        when(request.getParameter("templateid")).thenReturn(null);
+        when(request.getParameter("docid")).thenReturn(null);
 
         ProcessDocumentServlet processDocServlet = new ProcessDocumentServlet();
-        processDocServlet.setNotification(notificationEvents);
         processDocServlet.doGet(request,
                                 response);
 
-        assertEquals(1,
-                     receivedNotificationEvents.size());
-        Object event = receivedNotificationEvents.get(0);
-        assertTrue(event instanceof NotificationEvent);
-        NotificationEvent eventReceived = (NotificationEvent) event;
+        verify(loggingAppender).doAppend(loggingEventArgumentCaptor.capture());
+        final LoggingEvent loggingEvent = loggingEventArgumentCaptor.getValue();
+        assertEquals(Level.ERROR,
+                     loggingEvent.getLevel());
         assertEquals(MessageFormat.format(ProcessDocumentServlet.INVALID_PARAMS,
                                           "null",
                                           "null"),
-                     eventReceived.getNotification());
+                     loggingEvent.getFormattedMessage());
     }
 
     @Test
     public void testInvalidServerTemplate() throws Exception {
         when(kieServerIntegration.getServerClient(anyString())).thenReturn(null);
 
-        params.put("templateid",
-                   "invalidTemplateId");
-        params.put("docid",
-                   "invalidDocId");
-
-        TestHttpServletRequest request = new TestHttpServletRequest(params);
-        TestHttpServletResponse response = new TestHttpServletResponse();
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        when(request.getParameter("templateid")).thenReturn("invalidTemplateId");
+        when(request.getParameter("docid")).thenReturn("invalidDocId");
 
         ProcessDocumentServlet processDocServlet = new ProcessDocumentServlet();
-        processDocServlet.setNotification(notificationEvents);
         processDocServlet.setKieServerIntegration(kieServerIntegration);
         processDocServlet.doGet(request,
                                 response);
 
-        assertEquals(1,
-                     receivedNotificationEvents.size());
-        Object event = receivedNotificationEvents.get(0);
-        assertTrue(event instanceof NotificationEvent);
-        NotificationEvent eventReceived = (NotificationEvent) event;
+        verify(loggingAppender).doAppend(loggingEventArgumentCaptor.capture());
+        final LoggingEvent loggingEvent = loggingEventArgumentCaptor.getValue();
+        assertEquals(Level.ERROR,
+                     loggingEvent.getLevel());
         assertEquals(MessageFormat.format(ProcessDocumentServlet.INVALID_SERVICES_CLIENT,
                                           "invalidTemplateId"),
-                     eventReceived.getNotification());
+                     loggingEvent.getFormattedMessage());
     }
 
     @Test
@@ -136,28 +137,23 @@ public class ProcessDocumentServletTest {
         when(kieServicesClient.getServicesClient(any())).thenReturn(documentServicesClient);
         when(documentServicesClient.getDocument(anyString())).thenReturn(null);
 
-        params.put("templateid",
-                   "kie-server-id");
-        params.put("docid",
-                   "invalidDocId");
-
-        TestHttpServletRequest request = new TestHttpServletRequest(params);
-        TestHttpServletResponse response = new TestHttpServletResponse();
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        when(request.getParameter("templateid")).thenReturn("kie-server-id");
+        when(request.getParameter("docid")).thenReturn("invalidDocId");
 
         ProcessDocumentServlet processDocServlet = new ProcessDocumentServlet();
-        processDocServlet.setNotification(notificationEvents);
         processDocServlet.setKieServerIntegration(kieServerIntegration);
         processDocServlet.doGet(request,
                                 response);
 
-        assertEquals(1,
-                     receivedNotificationEvents.size());
-        Object event = receivedNotificationEvents.get(0);
-        assertTrue(event instanceof NotificationEvent);
-        NotificationEvent eventReceived = (NotificationEvent) event;
+        verify(loggingAppender).doAppend(loggingEventArgumentCaptor.capture());
+        final LoggingEvent loggingEvent = loggingEventArgumentCaptor.getValue();
+        assertEquals(Level.ERROR,
+                     loggingEvent.getLevel());
         assertEquals(MessageFormat.format(ProcessDocumentServlet.INVALID_DOCUMENT,
                                           "invalidDocId"),
-                     eventReceived.getNotification());
+                     loggingEvent.getFormattedMessage());
     }
 
     @Test
@@ -168,39 +164,45 @@ public class ProcessDocumentServletTest {
         when(documentInstance.getName()).thenReturn("testdoc.properties");
         when(documentInstance.getContent()).thenReturn("sampleContent".getBytes());
 
-        params.put("templateid",
-                   "kie-server-id");
-        params.put("docid",
-                   "AAAA-BBBB-CCCC-DDDD");
-
-        TestHttpServletRequest request = new TestHttpServletRequest(params);
-        TestHttpServletResponse response = new TestHttpServletResponse();
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        when(request.getParameter("templateid")).thenReturn("kie-server-id");
+        when(request.getParameter("docid")).thenReturn("AAAA-BBBB-CCCC-DDDD");
+        ProcessDocumentServletTest.StubServletOutputStream stubServletOutputStream = new ProcessDocumentServletTest.StubServletOutputStream();
+        when(response.getOutputStream()).thenReturn(stubServletOutputStream);
 
         ProcessDocumentServlet processDocServlet = new ProcessDocumentServlet();
-        processDocServlet.setNotification(notificationEvents);
         processDocServlet.setKieServerIntegration(kieServerIntegration);
         processDocServlet.doGet(request,
                                 response);
 
-        assertEquals(0,
-                     receivedNotificationEvents.size());
-
-        assertEquals("application/octet-stream",
-                     response.getContentType());
-
-        String docName = "";
-        String dispositionHeader = response.getHeader("Content-Disposition");
-        assertNotNull(dispositionHeader);
-        int index = dispositionHeader.indexOf("filename=");
-        if (index > 0) {
-            docName = dispositionHeader.substring(index + 10,
-                                                  dispositionHeader.length() - 1);
-        }
-        assertNotNull(docName);
-        assertEquals("testdoc.properties",
-                     docName);
         assertNotNull(response.getOutputStream());
+
         assertEquals("sampleContent",
-                     new String(response.getContent()));
+                     new String(((ProcessDocumentServletTest.StubServletOutputStream) response.getOutputStream()).getContent()));
+    }
+
+    protected class StubServletOutputStream extends ServletOutputStream {
+
+        public ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+        public byte[] getContent() {
+            return baos.toByteArray();
+        }
+
+        @Override
+        public void write(int i) throws IOException {
+            baos.write(i);
+        }
+
+        @Override
+        public boolean isReady() {
+            return true;
+        }
+
+        @Override
+        public void setWriteListener(WriteListener writeListener) {
+
+        }
     }
 }
