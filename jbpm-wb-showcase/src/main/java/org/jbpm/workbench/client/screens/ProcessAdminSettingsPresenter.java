da70609558309f8550f05f7d63c8b0000c3fc1cd
Cristiano Nicolai <cristiano.nicolai@gmail.com>
2017-09-26 15:06:46 -0300

    Extend process admin showcase to allow specifying processes variables when starting multiple instances

--- jbpm-wb-showcase/src/main/java/org/jbpm/workbench/client/screens/ProcessAdminSettingsPresenter.java
+++ jbpm-wb-showcase/src/main/java/org/jbpm/workbench/client/screens/ProcessAdminSettingsPresenter.java
@@ -19,6 +19,7 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.Dependent;
 import javax.inject.Inject;
@@ -27,8 +28,9 @@ import com.google.common.collect.FluentIterable;
 import com.google.common.collect.Maps;
 import org.jboss.errai.common.client.api.Caller;
 import org.jbpm.workbench.api.ProcessAdminService;
-import org.jbpm.workbench.pr.model.ProcessSummary;
 import org.jbpm.workbench.client.i18n.ProcessAdminConstants;
+import org.jbpm.workbench.pr.model.ProcessDefinitionKey;
+import org.jbpm.workbench.pr.model.ProcessSummary;
 import org.jbpm.workbench.pr.service.ProcessRuntimeDataService;
 import org.kie.server.controller.api.model.spec.ServerTemplate;
 import org.kie.workbench.common.screens.server.management.service.SpecManagementService;
@@ -36,6 +38,8 @@ import org.uberfire.client.annotations.WorkbenchPartTitle;
 import org.uberfire.client.annotations.WorkbenchPartView;
 import org.uberfire.client.annotations.WorkbenchScreen;
 import org.uberfire.client.mvp.UberView;
+
+import static java.util.Collections.emptyList;
 
 @Dependent
 @WorkbenchScreen(identifier = ProcessAdminSettingsPresenter.SCREEN_ID)
@@ -83,19 +87,25 @@ public class ProcessAdminSettingsPresenter {
 
     public void generateMockInstances(final String serverTemplateId,
                                       final String processId,
-                                      int amountOfTasks) {
+                                      final Integer amountOfTasks,
+                                      final String correlationKey,
+                                      final List<ProcessVariableSummary> variables) {
         final ProcessSummary summary = processeSummaryMap.get(processId);
 
         if (summary == null) {
             return;
         }
+
+        final Map<String, Object> params = variables.stream().collect(Collectors.toMap(ProcessVariableSummary::getVariableName,
+                                                                                       ProcessVariableSummary::getVariableValue));
+
         instancesAdminServices.call(
                 p -> view.displayNotification(constants.ProcessInstancesSuccessfullyCreated()))
                 .generateMockInstances(serverTemplateId,
                                        summary.getDeploymentId(),
                                        summary.getProcessDefId(),
-                                       null,
-                                       null,
+                                       correlationKey,
+                                       params,
                                        amountOfTasks);
     }
 
@@ -118,11 +128,31 @@ public class ProcessAdminSettingsPresenter {
                               true);
     }
 
+    public void onProcessSelected(final String serverTemplateId,
+                                  final String processId) {
+        final ProcessSummary process = processeSummaryMap.get(processId);
+
+        processRuntimeDataService.call((ProcessSummary summary) -> {
+            if (summary == null || summary.getProcessVariables() == null) {
+                view.addProcessVariables(emptyList());
+            } else {
+                final List<ProcessVariableSummary> variables = summary.getProcessVariables().keySet().stream().map(k -> new ProcessVariableSummary(k,
+                                                                                                                                                   null)).collect(Collectors.toList());
+                view.addProcessVariables(variables);
+            }
+        }).getProcess(serverTemplateId,
+                      new ProcessDefinitionKey(serverTemplateId,
+                                               process.getDeploymentId(),
+                                               process.getProcessDefId()));
+    }
+
     public interface ProcessAdminSettingsView extends UberView<ProcessAdminSettingsPresenter> {
 
         void displayNotification(String text);
 
         void addServerTemplates(Set<String> serverTemplateId);
+
+        void addProcessVariables(List<ProcessVariableSummary> variables);
 
         void clearProcessList();
 
