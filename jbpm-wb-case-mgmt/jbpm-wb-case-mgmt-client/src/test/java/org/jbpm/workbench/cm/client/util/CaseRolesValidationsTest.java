305e45668a9cf9f6f11a01a6acee0ed0be4013bb
Bojan Sremac <bsremac@redhat.com>
2017-12-11 02:18:37 +0100

    [JBPM-6678] Case Roles: Add tests for case roles assignment at case start (#958)

--- jbpm-wb-case-mgmt/jbpm-wb-case-mgmt-client/src/test/java/org/jbpm/workbench/cm/client/util/CaseRolesValidationsTest.java
+++ jbpm-wb-case-mgmt/jbpm-wb-case-mgmt-client/src/test/java/org/jbpm/workbench/cm/client/util/CaseRolesValidationsTest.java
@@ -19,10 +19,13 @@ package org.jbpm.workbench.cm.client.util;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.HashMap;
+import java.util.ArrayList;
 
 import org.jboss.errai.ui.client.local.spi.TranslationService;
 import org.jbpm.workbench.cm.model.CaseDefinitionSummary;
 import org.jbpm.workbench.cm.model.CaseRoleAssignmentSummary;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -30,19 +33,20 @@ import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import static java.util.Collections.*;
-import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class CaseRolesValidationsTest {
 
     @Mock
-    TranslationService translationService;
+    private TranslationService translationService;
 
     @InjectMocks
-    CaseRolesValidations caseRolesValidations;
+    private CaseRolesValidations caseRolesValidations;
 
     @Test
-    public void testValidateRolesAssignments_SingleAssignmentUser() {
+    public void testValidateRolesAssignments_roleCardinalityReached() {
         final Map<String, Integer> role = Collections.singletonMap("test",
                                                                    1);
         final CaseDefinitionSummary cds = CaseDefinitionSummary.builder().roles(role).build();
@@ -50,42 +54,76 @@ public class CaseRolesValidationsTest {
         final List<CaseRoleAssignmentSummary> roles = singletonList(CaseRoleAssignmentSummary.builder().name("test").users(singletonList("user1")).build());
         final List<String> errors = caseRolesValidations.validateRolesAssignments(cds,
                                                                                   roles);
-        assertTrue(errors.isEmpty());
+        verify(translationService,
+               never()).format(anyString(),
+                               anyString(),
+                               anyInt());
+        assertThat(errors).isEmpty();
     }
 
     @Test
-    public void testValidateRolesAssignments_SingleAssignmentGroup() {
-        final Map<String, Integer> role = Collections.singletonMap("test",
-                                                                   1);
-        final CaseDefinitionSummary cds = CaseDefinitionSummary.builder().roles(role).build();
-
-        final List<CaseRoleAssignmentSummary> roles = singletonList(CaseRoleAssignmentSummary.builder().name("test").groups(singletonList("group1")).build());
-        final List<String> errors = caseRolesValidations.validateRolesAssignments(cds,
-                                                                                  roles);
-        assertTrue(errors.isEmpty());
-    }
-
-    @Test
-    public void testValidateRolesAssignments_InvalidAssignment() {
-        final Map<String, Integer> role = Collections.singletonMap("test",
-                                                                   1);
+    public void testValidateRolesAssignments_roleCardinalityExceeded() {
+        final String caseRole = "test";
+        final Integer caseRoleCardinality = 1;
+        final Map<String, Integer> role = Collections.singletonMap(caseRole,
+                                                                   caseRoleCardinality);
         final CaseDefinitionSummary cds = CaseDefinitionSummary.builder().roles(role).build();
 
         final List<CaseRoleAssignmentSummary> roles = singletonList(CaseRoleAssignmentSummary.builder().name("test").users(singletonList("user1")).groups(singletonList("group1")).build());
         final List<String> errors = caseRolesValidations.validateRolesAssignments(cds,
                                                                                   roles);
-        assertFalse(errors.isEmpty());
+        verify(translationService).format(eq("InvalidRoleAssignment"),
+                                          eq(caseRole),
+                                          eq(caseRoleCardinality));
+        assertThat(errors).hasSize(1);
     }
 
     @Test
-    public void testValidateRolesAssignments_MultipleAssignments() {
-        final Map<String, Integer> role = Collections.singletonMap("test",
-                                                                   -1);
+    public void testValidateRolesAssignments_multipleRolesCardinalityExceeded() {
+        final Map<String, Integer> roles = new HashMap<>();
+        final Integer roleCardinality = 1;
+        roles.put("caseRole1",
+                  roleCardinality);
+        roles.put("caseRole2",
+                  roleCardinality);
+
+        final CaseDefinitionSummary cds = CaseDefinitionSummary.builder().roles(roles).build();
+
+        final List<CaseRoleAssignmentSummary> roleAssignments = new ArrayList<>();
+        roleAssignments.add(CaseRoleAssignmentSummary.builder().name("caseRole1").users(singletonList("user1")).groups(singletonList("group1")).build());
+        final List<String> assignedUsers = new ArrayList<>();
+        assignedUsers.add("user2");
+        assignedUsers.add("user3");
+        roleAssignments.add(CaseRoleAssignmentSummary.builder().name("caseRole2").users(assignedUsers).build());
+
+        final List<String> errors = caseRolesValidations.validateRolesAssignments(cds,
+                                                                                  roleAssignments);
+        verify(translationService,
+               times(2)).format(eq("InvalidRoleAssignment"),
+                                anyString(),
+                                eq(roleCardinality));
+        assertThat(errors).hasSize(2);
+    }
+
+    @Test
+    public void testValidateRolesAssignments_roleWithInfiniteCardinality() {
+        final String caseRole = "test";
+        final Integer caseRoleCardinality = -1;
+        final Map<String, Integer> role = Collections.singletonMap(caseRole,
+                                                                   caseRoleCardinality);
         final CaseDefinitionSummary cds = CaseDefinitionSummary.builder().roles(role).build();
 
-        final List<CaseRoleAssignmentSummary> roles = singletonList(CaseRoleAssignmentSummary.builder().name("test").users(singletonList("user1")).groups(singletonList("group1")).build());
-        final List<String> errors = caseRolesValidations.validateRolesAssignments(cds,
-                                                                                  roles);
-        assertTrue(errors.isEmpty());
+        final CaseRoleAssignmentSummary caseRoleAssignmentSummaryMock = mock(CaseRoleAssignmentSummary.class);
+        final List<CaseRoleAssignmentSummary> roles = singletonList(caseRoleAssignmentSummaryMock);
+        when(caseRoleAssignmentSummaryMock.getName()).thenReturn(caseRole);
+        when(caseRoleAssignmentSummaryMock.getUsers()).thenReturn(null);
+
+        try{
+            caseRolesValidations.validateRolesAssignments(cds,
+                                                          roles);
+        } catch (NullPointerException e) {
+            Assert.fail("Case role cardinality = " + caseRoleCardinality
+                        + ", role assignments check should be skipped");
+        }
     }
 }
