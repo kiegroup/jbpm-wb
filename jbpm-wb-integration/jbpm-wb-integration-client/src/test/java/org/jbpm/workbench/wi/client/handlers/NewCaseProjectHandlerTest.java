f4dfbde354ec4998e400351bf416609ab2ee0c73
Cristiano Nicolai <cristiano.nicolai@gmail.com>
2018-04-30 23:41:19 +1000

    RHPAM-542 - New Case Project uses old dialog (#1104)

--- jbpm-wb-integration/jbpm-wb-integration-client/src/test/java/org/jbpm/workbench/wi/client/handlers/NewCaseProjectHandlerTest.java
+++ jbpm-wb-integration/jbpm-wb-integration-client/src/test/java/org/jbpm/workbench/wi/client/handlers/NewCaseProjectHandlerTest.java
@@ -16,6 +16,9 @@
 
 package org.jbpm.workbench.wi.client.handlers;
 
+import javax.enterprise.event.Event;
+import javax.enterprise.inject.Instance;
+
 import com.google.gwtmockito.GwtMockitoTestRunner;
 import org.guvnor.common.services.project.model.WorkspaceProject;
 import org.jboss.errai.common.client.api.Caller;
@@ -23,45 +26,73 @@ import org.jbpm.workbench.wi.casemgmt.service.CaseProjectService;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.kie.workbench.common.screens.library.client.screens.project.AddProjectPopUpPresenter;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.uberfire.client.callbacks.Callback;
 import org.uberfire.mocks.CallerMock;
 import org.uberfire.mocks.EventSourceMock;
+import org.uberfire.mocks.MockInstanceImpl;
+import org.uberfire.mvp.ParameterizedCommand;
 import org.uberfire.workbench.events.NotificationEvent;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.*;
 
 @RunWith(GwtMockitoTestRunner.class)
 public class NewCaseProjectHandlerTest {
 
     @Mock
-    private CaseProjectService caseProjectService;
-    private Caller<CaseProjectService> caseProjectServiceCaller;
+    CaseProjectService caseProjectService;
 
-    @Mock
-    private EventSourceMock<NotificationEvent> notification;
+    Caller<CaseProjectService> caseProjectServiceCaller;
 
-    private NewCaseProjectHandler newCaseProjectHandler;
+    @Spy
+    Event<NotificationEvent> notification = new EventSourceMock<>();
+
+    @Spy
+    Instance<AddProjectPopUpPresenter> addProjectPopUpPresenterProvider = new MockInstanceImpl<>();
+
+    @InjectMocks
+    NewCaseProjectHandler newCaseProjectHandler;
 
     @Before
     public void setup() {
         caseProjectServiceCaller = new CallerMock<>(caseProjectService);
 
-        newCaseProjectHandler = new NewCaseProjectHandler();
         newCaseProjectHandler.setCaseProjectService(caseProjectServiceCaller);
         newCaseProjectHandler.setNotification(notification);
+        doNothing().when(notification).fire(any());
     }
 
     @Test
-    public void configureCaseProjectCallbackTest() {
+    public void testNewCaseProject() {
         final WorkspaceProject project = mock(WorkspaceProject.class);
-        final Callback<WorkspaceProject> creationSuccessCallback = mock(Callback.class);
+        final ParameterizedCommand<WorkspaceProject> creationSuccessCallback = mock(ParameterizedCommand.class);
+        AddProjectPopUpPresenter addProjectPopUpPresenterMock = mock(AddProjectPopUpPresenter.class);
+        when(addProjectPopUpPresenterProvider.get()).thenReturn(addProjectPopUpPresenterMock);
+        when(addProjectPopUpPresenterMock.getProjectCreationSuccessCallback()).thenReturn(creationSuccessCallback);
+        doAnswer(invocation -> {
+            ((ParameterizedCommand<WorkspaceProject>) invocation.getArguments()[0]).execute(project);
+            return null;
+        }).when(addProjectPopUpPresenterMock).setSuccessCallback(any());
 
-        newCaseProjectHandler.setCreationSuccessCallback(creationSuccessCallback);
-        newCaseProjectHandler.configureCaseProjectCallback.callback(project);
+        final Callback successCallback = mock(Callback.class);
+        newCaseProjectHandler.setCreationSuccessCallback(successCallback);
 
-        verify(creationSuccessCallback).callback(project);
+        newCaseProjectHandler.init();
+
+        verify(addProjectPopUpPresenterMock).setSuccessCallback(any());
+        verify(addProjectPopUpPresenterMock).show();
+
         verify(caseProjectService).configureNewCaseProject(project);
+
+        verify(successCallback).callback(project);
+        verify(creationSuccessCallback).execute(project);
+
+        verify(notification).fire(any());
+
+        verify(addProjectPopUpPresenterProvider).destroy(addProjectPopUpPresenterMock);
     }
 }
