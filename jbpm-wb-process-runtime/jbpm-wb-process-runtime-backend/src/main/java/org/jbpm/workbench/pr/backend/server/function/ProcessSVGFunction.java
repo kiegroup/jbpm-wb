86523c33bfe73be5d4a0ece59a564a24694beeea
William Siqueira <william.fatecsjc@gmail.com>
2020-10-27 13:33:53 -0300

    Improvements

--- jbpm-wb-process-runtime/jbpm-wb-process-runtime-backend/src/main/java/org/jbpm/workbench/pr/backend/server/function/ProcessSVGFunction.java
+++ jbpm-wb-process-runtime/jbpm-wb-process-runtime-backend/src/main/java/org/jbpm/workbench/pr/backend/server/function/ProcessSVGFunction.java
@@ -22,33 +22,33 @@ import javax.enterprise.context.Dependent;
 import javax.inject.Inject;
 
 import org.dashbuilder.external.impl.BackendComponentFunction;
-import org.jbpm.workbench.pr.backend.server.RemoteProcessImageServiceImpl;
+import org.jbpm.workbench.pr.service.ProcessImageService;
 
 @Dependent
-public class ProcessSVGFunction  implements BackendComponentFunction {
-    
+public class ProcessSVGFunction implements BackendComponentFunction {
+
     private final String CONTAINERID_PARAM = "containerId";
     private final static String PROCESSID_PARAM = "processId";
     private final static String SERVER_TEMPLATE_PARAM = "serverTemplate";
-    
+
     @Inject
-    RemoteProcessImageServiceImpl remoteProccessImageService;
+    ProcessImageService remoteProccessImageService;
 
     @Override
     public Object exec(Map<String, Object> params) {
         String containerId = getRequiredParam(CONTAINERID_PARAM, params);
         String processId = getRequiredParam(PROCESSID_PARAM, params);
         String serverTemplateId = getRequiredParam(SERVER_TEMPLATE_PARAM, params);
-        
+
         String processDiagram = remoteProccessImageService.getProcessDiagram(serverTemplateId, containerId, processId);
-        
+
         if (processDiagram == null) {
             throw new RuntimeException("Process SVG not found.");
         }
-        
+
         return processDiagram;
     }
-    
+
     private String getRequiredParam(String param, Map<String, Object> params) {
         Object value = params.get(param);
         if (value == null || value.toString().trim().isEmpty()) {
